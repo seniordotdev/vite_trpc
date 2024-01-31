f0b44f75c3604467d78950bf626b2800b053310d
Egamberdi Ibrohimov <seniordotdev@gmail.com>
2024-01-31 07:14:28 +0500

    Test deploy

---
+++ apps/server/src/main.ts
@@ -0,0 +1,19 @@
+import express, { Application, NextFunction, Request, Response } from "express";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { appRouter } from "./router";
+import cors from "cors";
+
+const app: Application = express();
+app.use(cors());
+
+app.use(
+	"/trpc",
+	trpcExpress.createExpressMiddleware({
+		router: appRouter,
+	})
+);
+
+const PORT: number = Number(process.env.PORT) || 5000;
+app.listen(PORT, () => {
+	console.log(`🚀 Server running on Port: ${PORT}`);
+});
